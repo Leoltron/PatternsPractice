c9327f687fa69e5aee4a79357c3355d55426c2de
Leoltron <leoole5@gmail.com>
2019-10-23 00:53:40 +0500

    Initial commit

---
+++ AbstractFactory/Factory.cs
@@ -0,0 +1,107 @@
+﻿namespace AbstractFactory
+{
+    public abstract class Body
+    {
+    }
+
+    public abstract class Engine
+    {
+    }
+
+    public abstract class Interior
+    {
+    }
+
+    public abstract class Windows
+    {
+    }
+
+    public abstract class Headlights
+    {
+    }
+
+    public abstract class AbstractCarFactory
+    {
+        public abstract Body CreateBody();
+        public abstract Engine CreateEngine();
+        public abstract Interior CreateInterior();
+        public abstract Windows CreateWindows();
+        public abstract Headlights CreateHeadlights();
+    }
+
+    public class BmwBody : Body
+    {
+    }
+
+    public class BmwEngine : Engine
+    {
+    }
+
+    public class BmwInterior : Interior
+    {
+    }
+
+    public class BmwWindows : Windows
+    {
+    }
+
+    public class BmwHeadlights : Headlights
+    {
+    }
+
+    public class BmwCarFactory : AbstractCarFactory
+    {
+        public override Body CreateBody() => new BmwBody();
+        public override Engine CreateEngine() => new BmwEngine();
+        public override Interior CreateInterior() => new BmwInterior();
+        public override Windows CreateWindows() => new BmwWindows();
+        public override Headlights CreateHeadlights() => new BmwHeadlights();
+    }
+
+    public class AudiBody : Body
+    {
+    }
+
+    public class AudiEngine : Engine
+    {
+    }
+
+    public class AudiInterior : Interior
+    {
+    }
+
+    public class AudiWindows : Windows
+    {
+    }
+
+    public class AudiHeadlights : Headlights
+    {
+    }
+
+    public class AudiCarFactory : AbstractCarFactory
+    {
+        public override Body CreateBody() => new AudiBody();
+        public override Engine CreateEngine() => new AudiEngine();
+        public override Interior CreateInterior() => new AudiInterior();
+        public override Windows CreateWindows() => new AudiWindows();
+        public override Headlights CreateHeadlights() => new AudiHeadlights();
+    }
+
+    public class Car
+    {
+        public Body Body { get; }
+        public Engine Engine { get; }
+        public Interior Interior { get; }
+        public Windows Windows { get; }
+        public Headlights Headlights { get; }
+
+        public Car(AbstractCarFactory carFactory)
+        {
+            Body = carFactory.CreateBody();
+            Engine = carFactory.CreateEngine();
+            Interior = carFactory.CreateInterior();
+            Windows = carFactory.CreateWindows();
+            Headlights = carFactory.CreateHeadlights();
+        }
+    }
+}
---
+++ Builder/MailBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Builder
+{
+    public class Mail
+    {
+        public string To { get; }
+        public string[] CopyTo { get; }
+        public string Subject { get; }
+        public string Body { get; }
+
+        public Mail(string to, string[] copyTo, string subject, string body)
+        {
+            To = to;
+            CopyTo = copyTo;
+            Subject = subject;
+            Body = body;
+        }
+    }
+
+    public class MailBuilder
+    {
+        private readonly string to;
+        private readonly string body;
+        private string subject = string.Empty;
+        private readonly List<string> copyTo = new List<string>();
+
+        public MailBuilder(string to, string body)
+        {
+            this.to = to;
+            this.body = body;
+        }
+
+        public MailBuilder AddCopyTo(string receiver)
+        {
+            copyTo.Add(receiver);
+            return this;
+        }
+
+        public MailBuilder SetSubject(string newSubject)
+        {
+            subject = newSubject;
+            return this;
+        }
+
+        public Mail Build()
+        {
+            return new Mail(to, copyTo.ToArray(), subject, body);
+        }
+    }
+}
