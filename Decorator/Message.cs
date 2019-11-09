ef86eccc9c67c7e4c74b07d1e7e123968d851c7d
Leoltron <leoole5@gmail.com>
2019-11-09 20:02:45 +0500

    Implemented decorators

---
+++ Decorator/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace Decorator
+{
+    public class Message
+    {
+        public string Author { get; }
+        public string Recipient { get; }
+        public string Text { get; }
+
+        public Message(string author, string recipient, string text)
+        {
+            Author = author;
+            Recipient = recipient;
+            Text = text;
+        }
+    }
+}
