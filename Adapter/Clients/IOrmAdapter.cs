6c2e40f7c6f7003344a113f6cbc9ddae6a4b63b5
Leoltron <leoole5@gmail.com>
2019-11-09 18:05:30 +0500

    Implemented adapter

--- Adapter/Clients/IOrmAdapter.cs
+++ Adapter/Clients/IOrmAdapter.cs
@@ -1,6 +1,10 @@
-﻿namespace Adapter.Clients
+﻿using Adapter.FirstOrmLibrary;
+using Adapter.Models;
+using Adapter.SecondOrmLibrary;
+
+namespace Adapter.Clients
 {
-    public interface IOrmAdapter // ITarget
+    public interface IOrmAdapter : IFirstOrm<DbUserInfoEntity>, IFirstOrm<DbUserEntity>, ISecondOrm
     {
     }
 }
--- Adapter/Clients/UserClient.cs
+++ Adapter/Clients/UserClient.cs
@@ -7,73 +7,38 @@ namespace Adapter.Clients
 {
     public class UserClient
     {
-        private IOrmAdapter _ormAdapter;
+        private readonly IOrmAdapter ormAdapter;
 
-        private IFirstOrm<DbUserEntity> _firstOrm1;
-        private IFirstOrm<DbUserInfoEntity> _firstOrm2;
-
-        private ISecondOrm _secondOrm;
-
-        private bool _useFirstOrm = true;
+        private readonly IFirstOrm<DbUserEntity> firstOrm1;
+        private readonly IFirstOrm<DbUserInfoEntity> firstOrm2;
 
         public (DbUserEntity, DbUserInfoEntity) Get(int userId)
         {
-            if (_useFirstOrm)
-            {
-                var user = _firstOrm1.Read(userId);
-                var userInfo = _firstOrm2.Read(user.InfoId);
-                return (user, userInfo);
-            }
-            else
-            {
-                var user = _secondOrm.Context.Users.First(i => i.Id == userId);
-                var userInfo = _secondOrm.Context.UserInfos.First(i => i.Id == user.InfoId);
-                return (user, userInfo);
-            }
-
-            // you should return DbUserEntity via _ormAdapter
-            return (null, null);
+            var user = firstOrm1.Read(userId);
+            var userInfo = firstOrm2.Read(user.InfoId);
+            return (user, userInfo);
         }
 
         public void Add(DbUserEntity user, DbUserInfoEntity userInfo)
         {
-            if (_useFirstOrm)
-            {
-                _firstOrm1.Add(user);
-                _firstOrm2.Add(userInfo);
-            }
-            else
-            {
-                _secondOrm.Context.Users.Add(user);
-                _secondOrm.Context.UserInfos.Add(userInfo);
-            }
-
-            // you should create DbUserEntity and DbUserInfoEntity via _ormAdapter
+            ormAdapter.Add(user);
+            ormAdapter.Add(userInfo);
         }
 
         public void Remove(int userId)
         {
-            if (_useFirstOrm)
-            {
-                var user = _firstOrm1.Read(userId);
-                var userInfo = _firstOrm2.Read(user.InfoId);
+            var user = firstOrm1.Read(userId);
+            var userInfo = firstOrm2.Read(user.InfoId);
 
-                _firstOrm2.Delete(userInfo);
-                _firstOrm1.Delete(user);
-            }
-            else
-            {
-                var user = _secondOrm.Context.Users.First(i => i.Id == userId);
-                _secondOrm.Context.Users.Remove(user);
-                _secondOrm.Context.UserInfos.RemoveWhere(i => i.Id == user.InfoId);
-            }
-
-            // you should remove DbUserEntity and DbUserInfoEntity via _ormAdapter
+            firstOrm2.Delete(userInfo);
+            firstOrm1.Delete(user);
         }
 
         public UserClient(IOrmAdapter ormAdapter)
         {
-            _ormAdapter = ormAdapter;
+            this.ormAdapter = ormAdapter;
+            firstOrm1 = ormAdapter;
+            firstOrm2 = ormAdapter;
         }
     }
 }
