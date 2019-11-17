69558db3eda74d7f71cd22cad1f38e5b7e82aff1
Leoltron <leoole5@gmail.com>
2019-11-17 17:30:30 +0500

    Added IChatClient implementation

---
+++ Decorator/ChatServer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Decorator
+{
+    public class ChatServer
+    {
+        private readonly Dictionary<string, List<Message>> messageInbox = new Dictionary<string, List<Message>>();
+
+        public void Send(Message message)
+        {
+            if (!messageInbox.TryGetValue(message.Recipient, out var messages))
+            {
+                messageInbox[message.Recipient] = messages = new List<Message>();
+            }
+
+            messages.Add(message);
+        }
+
+        public IEnumerable<Message> RetrieveMessages(string clientName)
+        {
+            if (!messageInbox.TryGetValue(clientName, out var inbox)) return Enumerable.Empty<Message>();
+            var messages = inbox.ToList();
+            inbox.Clear();
+            return messages;
+        }
+    }
+}
